3332c12f129de41edd1439d4efdc06f45b54ac08
Asmaa231 <asmaasalem231@gmail.com>
2024-03-26 08:41:57 -0700

    a

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -4,18 +4,19 @@
 """
 import sys
 
-    def fizzbuzz(n):
-        """
-            FizzBuzz function prints numbers from 1 to n separated by a space.
-            - For multiples of three print "Fizz" instead of the number and for
-              multiples of five print "Buzz".
-            - For numbers which are multiples of both three and
-            five print "FizzBuzz".
-        """
+
+def fizzbuzz(n):
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+    
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
     if n < 1:
         return
-    tmp_result = []
 
+    tmp_result = []
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
@@ -25,7 +26,8 @@ import sys
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
-            print(" ".join(tmp_result))
+    print(" ".join(tmp_result))
+
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
@@ -33,5 +35,6 @@ if __name__ == '__main__':
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
-        number = int(sys.argv[1])
-        fizzbuzz(number)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
